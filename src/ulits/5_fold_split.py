1a9895038d5c023685b10f76b7478288a4edc171
Libo Qin <yizhen@tanlibodeMacBook-Pro.local>
2017-11-30 15:57:36 +0800

    add 5_fold_split

---
+++ src/ulits/5_fold_split.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+from __future__ import division
+import argparse
+import random
+
+def split_data(args):
+    """
+    split 5_fold data
+    :param args:
+    :return:
+    """
+    origin_path = args.origin_data_path
+    fp_origin = open(origin_path, encoding='utf-8')
+    path_directory = args.path
+
+    origin_content = fp_origin.read().strip().split('\n')
+    number = len(origin_content)
+
+    shuffle_content = random.shuffle(origin_content)
+    name_str_train = "train_fold_"
+    name_str_test = "test_fold_"
+    print(number)
+    interval1 = int(number) // 5
+    for i in range(5):
+        if i == 4:
+            end_interval = number
+        else:
+            end_interval = (i + 1) * interval1
+        test_data = origin_content[i*interval1:end_interval]
+        train_data = origin_content[0:(i*interval1)]+origin_content[end_interval:number]
+        str_test = '\n'.join(test_data)
+        str_train = '\n'.join(train_data)
+        train_path = path_directory+name_str_train+str(i+1)+".txt"
+        test_path = path_directory+name_str_test+str(i+1)+".txt"
+        fp_train = open(train_path, 'w', encoding='utf-8')
+        fp_test = open(test_path, 'w', encoding='utf-8')
+        fp_train.write(str_train)
+        fp_test.write(str_test)
+        fp_train.close()
+        fp_test.close()
+
+    fp_origin.close()
+def main():
+    arparser = argparse.ArgumentParser("split data for 5_fold_experiment")
+    arparser.add_argument("--origin_data_path", help = "data to be splited", default="../../data/train.txt")
+    arparser.add_argument("--path", help = "path directory", default="../../outputs/")
+
+    args = arparser.parse_args()
+    split_data(args)
+
+if __name__ == '__main__':
+    main()
