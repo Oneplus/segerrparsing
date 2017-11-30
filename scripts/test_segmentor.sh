f486d77253209c48da6c6f1e875bc191aaef1045
Libo Qin <yizhen@tanlibodeMacBook-Pro.local>
2017-11-30 13:40:26 +0800

    fixed bugs

--- scripts/test_segmentor.sh
+++ scripts/test_segmentor.sh
@@ -6,4 +6,4 @@ python2 ../src/segmentor/train_model.py --type test \
 --model_save_path ../models/model.pkl \
 --unigram_embedding ../data/unigram_100_test.embed \
 --bigram_embedding  ../data/bigram_100_test.embed \
---res_path ../output/test_res.txt \
+--res_path ../outputs/test_res.txt \
