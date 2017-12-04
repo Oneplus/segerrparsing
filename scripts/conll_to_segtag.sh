16fdbf3e993eee5a0e316b5f3e6a34a62ee99341
Libo Qin <yizhen@tanlibodeMacBook-Pro.local>
2017-12-04 14:25:20 +0800

    fix bug in lacking use_cuda = use_cuda

--- scripts/conll_to_segtag.sh
+++ scripts/conll_to_segtag.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 prefix=outputs
-mkdir -p ./${prefix}/CTB5.1.seg/
+mkdir -p ../${prefix}/CTB5.1.seg/
 for fold in train devel test;
 do
-    python ./src/conll_to_segdat.py \
-        --input ./data/CTB5.1/dependency-penn2malt/CTB5.1-${fold}.gp.conll \
-        --output ./${prefix}/CTB5.1.seg/CTB5.1-${fold}.dat
+    python ../src/conll_to_segdat.py \
+        --input ../data/CTB5.1/CTB5.1-${fold}.gp.conll \
+        --output ../${prefix}/CTB5.1.seg/CTB5.1-${fold}.dat
 done
 echo "done."
