381f1120cb627ceecaa1f58fb646f650b61e86e3
Oneplus <oneplus.lau@gmail.com>
2018-02-27 22:01:19 +0800

    update

---
+++ src/tools/nfold_split_conll.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+from __future__ import division
+from __future__ import unicode_literals
+import os
+import argparse
+import random
+import codecs
+
+
+def main():
+  cmd = argparse.ArgumentParser("split data for 5_fold_conll experiment")
+  cmd.add_argument("--input", help="data to be splited")
+  cmd.add_argument("--output", help="path directory")
+  cmd.add_argument("--nfold", type=int, default=5, help="the number of folds")
+
+  opt = cmd.parse_args()
+
+  random.seed(1234)
+  dataset = codecs.open(opt.input, 'r', encoding='utf-8').read().strip().split('\n\n')
+  n_sent = len(dataset)
+
+  random.shuffle(dataset)
+
+  # print(number)
+  fold_size = n_sent // opt.nfold
+  basename = os.path.basename(opt.input)
+  for i in range(opt.nfold):
+    start, end = i * fold_size, min((i + 1) * fold_size, n_sent)
+    train_path = os.path.join(opt.output, '{0}.{1}.train'.format(basename, i))
+    test_path = os.path.join(opt.output, '{0}.{1}.test'.format(basename, i))
+
+    with codecs.open(train_path, 'w', encoding='utf-8') as fpo:
+      print('\n\n'.join([data for i, data in enumerate(dataset) if i < start or i >= end]), file=fpo)
+
+    with codecs.open(test_path, 'w', encoding='utf-8') as fpo:
+      print('\n\n'.join([data for i, data in enumerate(dataset) if start < i < end]), file=fpo)
+
+
+if __name__ == '__main__':
+    main()
