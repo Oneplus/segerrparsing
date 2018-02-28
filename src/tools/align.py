3e78be7c9a65c5a5b75dc7f30413c4656a395728
Oneplus <oneplus.lau@gmail.com>
2018-02-28 19:47:44 +0800

    update

--- src/tools/align.py
+++ src/tools/align.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from __future__ import print_function
+from __future__ import unicode_literals
 import sys
 import argparse
 import codecs
@@ -16,80 +18,34 @@ class CoNLLLine(object):
     self.end = end
 
 
-def load_conll_data(path, skip_abnormal=True):
-  # exceptions:
-  # - one gold sentence has different segmentation
-  # - one gold sentence has one segmentation, but different postag or dependencies
-  output = {}
-  with codecs.open(path, 'r', encoding='utf-8') as f:
-    dataset = f.read().strip().split('\n\n')
-    for data in dataset:
-      lines = data.split('\n')
-      abnormal = any([len(line.split()) != 8 for line in lines])
-      if abnormal:
-        print('ABNORMAL:\n{0}'.format(data), file=sys.stderr)
-        if skip_abnormal:
-          continue
-      key = ''.join([line.split()[1] for line in lines])
-      if key in output:
-        output[key].append(data)
-      else:
-        output[key] = [data]
-  return output
-
-
-def load_segment_data(path):
-  output = {}
-  with codecs.open(path, 'r', encoding='utf-8') as f:
-    for line in f:
-      data = line.strip()
-      key = ''.join(data.split())
-      if key in output:
-        output[key].append(data)
-      else:
-        output[key] = [data]
-  return output
-
-
-def sanity_check(conll_data, segment_data):
-  conll_keys = conll_data.keys()
-  segment_keys = segment_data.keys()
-  # print(list(set(conll_keys) - set(segment_keys)))
-  # print(list(set(segment_keys) - set(conll_keys)))
-  assert (set(conll_keys) & set(segment_keys)) == set(conll_keys)
-
-  for key in conll_keys:
-    assert len(conll_data[key]) == len(segment_data[key])
-    for inst in conll_data[key]:
-      lines = inst.splitlines()
-      for i, line in enumerate(lines):
-        tokens = line.split()
-        assert len(tokens) == 8, '{0}, {1}'.format(inst, key)
-        assert int(tokens[0]) == i + 1
-
-
 def print_one_line(entry, handler):
-  print('{id}\t{form}\t{form}\t{pos}\t{pos}\t_\t{head}\t{deprel}\t_\t_'.format(
+  print('{id}\t{form}\t{form}\t{pos}\t{pos}\t{pos}\t{head}\t{deprel}\t_\t_'.format(
     id=entry.id, form=entry.form, pos=entry.postag, head=entry.head, deprel=entry.deprel), file=handler)
 
 
-def align_one_instance(conll_inst, segment_inst):
-  inst = [line.split() for line in conll_inst.splitlines()]
+def align_one_instance(gold_data, auto_data):
+  inst = [line.split() for line in gold_data.splitlines()]
   start_id = 0
   new_inst = []
   for token in inst:
+    if len(token) == 6:
+      token.insert(1, '\u3000')
+      token.insert(2, '\u3000')
     end_id = start_id + len(token[1])
     new_inst.append(CoNLLLine(id=token[0], form=token[1], postag=token[3], head=token[6], deprel=token[7],
                               start=start_id, end=end_id))
     start_id = end_id
   gold_inst = new_inst
 
-  inst = segment_inst.split()
+  inst = [line.split() for line in auto_data.splitlines()]
   new_inst = []
   start_id = 0
   for i, token in enumerate(inst):
-    end_id = start_id + len(token)
-    new_inst.append(CoNLLLine(id=str(i + 1), form=token, postag=None, head=None, deprel=None,
+    if len(token) == 6:
+      token.insert(1, '\u3000')
+      token.insert(2, '\u3000')
+    end_id = start_id + len(token[1])
+    new_inst.append(CoNLLLine(id=str(i + 1), form=token[1], postag=None, head=None, deprel=None,
                               start=start_id, end=end_id))
     start_id = end_id
   auto_inst = new_inst
@@ -156,28 +112,26 @@ def align_one_instance(conll_inst, segment_inst):
   return auto_inst
 
 
-def align(conll_data, segment_data, output):
-  keys = conll_data.keys()
-  handler = codecs.open(output, 'w', encoding='utf-8')
-  for key in keys:
-    conll_instances = conll_data[key]
-    segment_instances = segment_data[key]
-    for conll_inst, segment_inst in zip(conll_instances, segment_instances):
-      new_inst = align_one_instance(conll_inst, segment_inst)
-      for entry in new_inst:
-        print_one_line(entry, handler)
-      print('', file=handler)
+def align(gold_data, auto_data, output):
+  if output is not None:
+    handler = codecs.open(output, 'w', encoding='utf-8')
+  else:
+    handler = codecs.getwriter('utf-8')(sys.stdout)
+  for gold_inst, auto_inst in zip(gold_data, auto_data):
+    new_inst = align_one_instance(gold_inst, auto_inst)
+    for entry in new_inst:
+      print_one_line(entry, handler)
+    print('', file=handler)
 
 
 if __name__ == '__main__':
-    cmd = argparse.ArgumentParser('align auto sentence and gold sentence')
-    cmd.add_argument("--gold_conll", help="path of gold conll",default='../../data/conll/CTB5.1-devel.gp.conll')
-    cmd.add_argument("--auto_seg", help="path of aligned sentence",default='../../data/conll/auto_devel.sentence')
-    cmd.add_argument("--output", help="path of auto sentence",default='../../data/conll/output.conll')
-    args = cmd.parse_args()
+  cmd = argparse.ArgumentParser('align auto sentence and gold sentence')
+  cmd.add_argument("--gold", help="path of gold conll")
+  cmd.add_argument("--auto", help="path of aligned sentence")
+  cmd.add_argument("--output", help="path of auto sentence")
+  opt = cmd.parse_args()
 
-    conll_data = load_conll_data(args.gold_conll)
-    autoseg_data = load_segment_data(args.auto_seg)
+  gold_data = codecs.open(opt.gold, 'r').read().strip().split('\n\n')
+  auto_data = codecs.open(opt.auto, 'r').read().strip().split('\n\n')
 
-    sanity_check(conll_data, autoseg_data)
-    align(conll_data, autoseg_data, args.output)
+  align(gold_data, auto_data, opt.output)
