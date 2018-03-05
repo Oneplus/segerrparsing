9573aff45836722756dd41e03d8bf4a729b8bb3d
Oneplus <oneplus.lau@gmail.com>
2018-03-05 13:58:21 +0800

    update

--- src/seqlabel/modules.py
+++ src/seqlabel/modules.py
@@ -150,6 +150,8 @@ class PartialClassifyLayer(ClassifyLayer):
 
 
 class CRFLayer(nn.Module):
+  ninf = -1e8
+
   def __init__(self, n_in, num_tags, use_cuda=False):
     super(CRFLayer, self).__init__()
     self.n_in = n_in
@@ -203,19 +205,34 @@ class CRFLayer(nn.Module):
     return self._log_sum_exp(log_prob, 1)
 
   def _viterbi_decode(self, emissions):
+    seq_length, batch_size = emissions.size(0), emissions.size(1)
+    mask = torch.ones(seq_length, batch_size, self.num_tags).float()
+
+    # create mask
+    for i in range(seq_length):
+      for j in range(batch_size):
+          mask[i][j][0] = 0
+
+    mask = Variable(mask)
+    return self._viterbi_decode_with_mask(emissions, mask)
+
+  def _viterbi_decode_with_mask(self, emissions, mask):
     seq_length = emissions.size(0)
 
     # (batch_size, num_tags)
-    viterbi_score = torch.zeros_like(emissions[0]) + emissions[0]
+    viterbi_score = emissions[0] * mask[0] + (1 - mask[0]) * self.ninf
     viterbi_path = []
     for i in range(1, seq_length):
+      prev_mask, cur_mask = mask[i - 1], mask[i]
+      transition_mask = torch.bmm(prev_mask.unsqueeze(2), cur_mask.unsqueeze(1))
+
       # (batch_size, num_tags, 1)
-      broadcast_score = viterbi_score.unsqueeze(2)
-      # (1, num_tags, num_tags)
-      broadcast_transitions = self.transitions.unsqueeze(0)
-      # (batch_size, 1, num_tags)
-      broadcast_emission = emissions[i].unsqueeze(1)
+      broadcast_score = viterbi_score.unsqueeze(2) * prev_mask.unsqueeze(2) + (1 - prev_mask.unsqueeze(2)) * self.ninf
       # (batch_size, num_tags, num_tags)
+      broadcast_transitions = self.transitions.unsqueeze(0) * transition_mask + (1 - transition_mask) * self.ninf
+      # (batch_size, 1, num_tags)
+      broadcast_emission = emissions[i].unsqueeze(1) * cur_mask.unsqueeze(1) + (1 - cur_mask.unsqueeze(1)) * self.ninf
+
       score = broadcast_score + broadcast_transitions + broadcast_emission
       # (batch_size, num_tags), (batch_size, num_tags)
       best_score, best_path = score.max(1)
@@ -247,7 +264,6 @@ class CRFLayer(nn.Module):
 
 class PartialCRFLayer(CRFLayer):
   uncertain = 1
-  ninf = -1e8
 
   def __init__(self, n_in, num_tags, use_cuda=False):
     super(PartialCRFLayer, self).__init__(n_in, num_tags, use_cuda)
@@ -289,19 +305,29 @@ class PartialCRFLayer(CRFLayer):
       prev_mask, cur_mask = mask[i - 1], mask[i]
       transition_mask = torch.bmm(prev_mask.unsqueeze(2), cur_mask.unsqueeze(1))
       # (batch_size, num_tags, 1)
-      broadcast_log_prob = log_prob.unsqueeze(2) * prev_mask.unsqueeze(2) +\
-                           (1 - prev_mask.unsqueeze(2)) * self.ninf
+      broadcast_log_prob = log_prob.unsqueeze(2) * prev_mask.unsqueeze(2) + (1 - prev_mask.unsqueeze(2)) * self.ninf
       # (batch_size, num_tags, num_tags)
-      broadcast_transitions = self.transitions.unsqueeze(0) * transition_mask +\
-                              (1 - transition_mask) * self.ninf
+      broadcast_transitions = self.transitions.unsqueeze(0) * transition_mask + (1 - transition_mask) * self.ninf
       # (batch_size, 1, num_tags)
-      broadcast_emissions = emissions[i].unsqueeze(1) * cur_mask.unsqueeze(1) +\
-                            (1 - cur_mask.unsqueeze(1)) * self.ninf
+      broadcast_emissions = emissions[i].unsqueeze(1) * cur_mask.unsqueeze(1) + (1 - cur_mask.unsqueeze(1)) * self.ninf
 
       score = broadcast_log_prob + broadcast_transitions + broadcast_emissions
       log_prob = self._log_sum_exp(score, 1)
 
     return self._log_sum_exp(log_prob, 1)
+
+  def _viterbi_decode(self, emissions):
+    seq_length, batch_size = emissions.size(0), emissions.size(1)
+    mask = torch.ones(seq_length, batch_size, self.num_tags).float()
+
+    # create mask
+    for i in range(seq_length):
+      for j in range(batch_size):
+        mask[i][j][0] = 0
+        mask[i][j][self.uncertain] = 0
+
+    mask = Variable(mask)
+    return self._viterbi_decode_with_mask(emissions, mask)
 
 
 class EmbeddingLayer(nn.Module):
